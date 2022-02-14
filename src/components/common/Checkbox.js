8ae33f0b92e63fdf1febc2ec02c73124683bea4b
Shoaib <shoaib.mehedi@gmail.com>
2022-02-14 10:44:28 +0600

    selection fixed

--- src/components/common/Checkbox.js
+++ src/components/common/Checkbox.js
@@ -4,15 +4,15 @@ import { taskStatus } from "../../constants/constants";
 
 const Checkbox = ({ label, value, onChange,disable,api }) => {
 
-  const [checked, setChecked] = React.useState(value === taskStatus.COMPLETED);
+  // const [checked, setChecked] = React.useState(value === taskStatus.COMPLETED);
   const handleChangee = (e) => {
-    setChecked(!checked);
+    // setChecked(!checked);
     console.log(api);
     api();
   };
     return (
       <label className={'text-xl '+(disable === taskStatus.COMPLETED?'text-slate-500':'')}>
-        <input className={'p-4 text-2xl mr-4'} type="checkbox" checked={checked} onChange={handleChangee} disabled={disable === taskStatus.COMPLETED}  />
+        <input className={'p-4 text-2xl mr-4'} type="checkbox" checked={value === taskStatus.COMPLETED} onChange={handleChangee} disabled={disable === taskStatus.COMPLETED}  />
         {label}
       </label>
     );
